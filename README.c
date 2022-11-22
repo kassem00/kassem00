7dff2cbd1d52f37f552ec50449a270e618b174a4
kassem00 <66543331+kassem00@users.noreply.github.com>
2022-11-22 10:50:12 +0200

    Update and rename README.md to README.c

---
+++ README.c
@@ -0,0 +1,16 @@
+#include <kassem.h>//😊
+int main(){
+personality_t kasem;
+kasem.Hobby={"electronics","coding","Readig"};//🤖 🤖 🤖 🤖 🤖 🤖 
+while(1){//😉 😌|| ☠️
+if(kassem.live==1){//😉 😌
+human_action(kasem.Establish_prayer);//😌 😍
+human_action(kasem.Hobby);//😵‍💫🤓
+}else{//☠️
+printf("قُلْ إِنَّ صَلَاتِي وَنُسُكِي وَمَحْيَايَ وَمَمَاتِي لِلَّهِ رَبِّ الْعَالَمِينَ");
+}
+}
+<!---
+kassem00/kassem00 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
--- README.md
+++
@@ -1,5 +0,0 @@
-
-<!---
-kassem00/kassem00 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
